81c86c02f40e74a62e52107dee8a6bf37224ca10
Ayomide Lawal <ayomidelawal700@gmail.com>
2024-06-11 12:50:10 +0100

    define type

--- main.go
+++ main.go
@@ -5,12 +5,34 @@ import (
 	"net/http"
 )
 
+type Server struct{}
+
+func (s Server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	switch r.URL.Path {
+	case "/":
+		handleFunc(w, r)
+
+	case "/contact":
+		contactHandler(w, r)
+	default:
+		http.Error(w, "Page not found", http.StatusNotFound)
+	}
+
+}
+
 func main() {
-	http.HandleFunc("/", handleFunc)
+	var server Server
 	fmt.Println("Server running on port:3000")
-	http.ListenAndServe(":3000", nil)
+	http.ListenAndServe(":3000", server)
 }
 
 func handleFunc(w http.ResponseWriter, _ *http.Request) {
+	w.Header().Set("Content-type", "text/html; charset=utf=8")
 	fmt.Fprint(w, "<h1>Welcome to my go love</h1>")
+}
+
+func contactHandler(w http.ResponseWriter, _ *http.Request) {
+	w.Header().Set("Content-type", "text/html; charset=utf-8")
+	fmt.Fprint(w, `<h1>Contact page</h1><p>To get in touch reach out to me
+	 <a href=\"mailto:ayomidelawal700@gmail.com\">ayomidelawal700@gmail.com</a></p>`)
 }
